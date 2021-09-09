5c32c177910586168f3c0e528131cfcf6aab2c23
10Numero <florian.gozard@edu.devinci.fr>
2021-09-09 15:35:18 +0200

    cablé petite pute

---
+++ GodfatherJam/Assets/_Game/Scripts/EventTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTrigger : MonoBehaviour
+{
+    public enum EventType
+    {
+        DEATH,
+        SPAWN
+    }
+
+    public EventType eventType;
+    public int id;
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.transform.name == "Player")
+        {
+            switch (eventType)
+                    {
+                        case EventType.DEATH:
+                            EventController.instance.DeathEvent();
+                            break;
+                        case EventType.SPAWN:
+                            EventController.instance.SpawnUpdateEvent(id,transform.position);
+                            break;
+                    }
+        }
+        
+    }
+}
---
+++ GodfatherJam/Assets/_Game/Scripts/MovingPortal.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MovingPortal : MonoBehaviour
+{
+    public Vector3 movingOffsetPoint;
+    public float movingTime;
+    public Transform movedObj;
+    private Vector3 pos;
+    private bool back;
+
+    void Start()
+    {
+        back = false;
+        pos = transform.localPosition + movingOffsetPoint;
+
+        SetPos();
+    }
+
+    void SetPos()
+    {
+        movedObj.DOLocalMove(pos, movingTime).OnComplete(() => NewPos());
+    }
+
+    void NewPos()
+    {
+        Debug.Log("New pos");
+
+        back = !back;
+
+        if (back)
+        {
+            pos = transform.localPosition - movingOffsetPoint;
+            SetPos();
+        }
+
+        else
+        {
+            pos = transform.localPosition + movingOffsetPoint;
+            SetPos();
+        }
+    }
+
+
+}
