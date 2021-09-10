27e5de76c1c7ff4f20fe925b1419cb4202a5f111
10Numero <florian.gozard@edu.devinci.fr>
2021-09-10 11:46:46 +0200

    auto build decal

---
+++ GodfatherJam/Assets/_Game/Scripts/AutoBuildDecal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DecalSystem;
+
+public class AutoBuildDecal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Decal>().BuildAndSetDirty();
+    }
+
+}
