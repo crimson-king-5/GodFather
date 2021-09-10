7cf82c4412657ee097f9601f3be6d1834062945d
10Numero <florian.gozard@edu.devinci.fr>
2021-09-10 02:46:54 +0200

    ARE U FCKING FIX NOW

--- GodfatherJam/Assets/_Game/Scripts/PauseController.cs
+++ GodfatherJam/Assets/_Game/Scripts/PauseController.cs
@@ -24,6 +24,7 @@ public class PauseController : MonoBehaviour
         menu.gameObject.SetActive(true);
         isPaused = true;
         Cursor.lockState = CursorLockMode.None;
+        SelectionRadialMenu.instance.radialMenuHolder.gameObject.SetActive(false);
     }
 
     public void Quit()
