295776fa0e1f1271887028fe1868011cd6181f29
10Numero <florian.gozard@edu.devinci.fr>
2021-09-10 02:14:22 +0200

    blur + menu + fix event controller win

---
+++ GodfatherJam/Assets/_Game/Scripts/PauseController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    public KeyCode pauseInput;
+    private bool isPaused;
+
+    private void Awake()
+    {
+        isPaused = false;
+    }
+    public void Pause()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Resume()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(pauseInput))
+        {
+
+        }
+    }
+}
